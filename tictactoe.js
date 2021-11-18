8d1ad11c3187b59b261cf25bb3e1fd84bc784638
Ismael Correia <ismaelavilacorreia@gmail.com>
2021-11-17 21:06:10 -0300

    up to decision

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,44 @@
-let player1 = 'X';
-let player2 = 'O';
-let player1selection = [];
-let player2selection = [];
+let currentPlayer = 'X';
+let nextPlayer = 'O';
+let playerXSelection = [];
+let playerOSelection = [];
+
+const handleClick = (event) => {
+    const cell = event.target;
+    
+    cell.innerHTML = currentPlayer;
+
+    if (currentPlayer === 'X') {
+        playerSelections = playerXSelection;
+        nextPlayer = 'O';
+    } else {
+        playerSelections = playerOSelection;
+        nextPlayer = 'X';
+    }
+
+    playerSelections.push(Number(cell.id));
+
+    if (checkWinner(playerSelections)) {
+        alert('Player '+currentPlayer+' wins!"');
+        resetGame();
+    }
+
+    if (checkDraw()) {
+        alert('Draw!!');
+        resetGame();
+    }
+
+
+    currentPlayer = nextPlayer;
+    console.log(cell.id);
+}
+
+const cells = document.querySelectorAll('td');
+
+for (i=0; i<cells.length; i++) {
+    cells[i].addEventListener('click', handleClick);
+}
+
 
 const winningCombos = [
     [1,2,3],
@@ -14,11 +51,31 @@ const winningCombos = [
     [3,5,7]
 ]
 
-const handleClick = function(event) {
-    const cell = event.target;
-    console.log(cell.id);
+
+function checkWinner(playerSelections) {
+    for (i=0; i<winningCombos.length; i++) {
+        let matches = 0;
+        for (j=0; j<winningCombos[i].length; j++) {
+            if (playerSelections.includes(winningCombos[i][j])) {
+                matches++
+            } else break
+            if (matches === winningCombos[i].length) {
+                return true
+            }
+        }
+    }
+    return false
 }
-const cells = document.querySelectorAll('td');
-for (i=0; i<cells.length; i++) {
-    cells[i].addEventListener('click', handleClick);
+
+function checkDraw() {
+    return (playerXSelection.length + playerOSelection.length) >= cells.length;
 }
+
+function resetGame() {
+    playerXSelection = new Array();
+    playerOSelection = new Array();
+    for (i=0; i<cells.length; i++) {
+        cells[i].innerHTML = '';
+    }
+}
+
